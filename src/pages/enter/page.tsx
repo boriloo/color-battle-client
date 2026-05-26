15ad9ebe0d56f14fe4cdcb3e5e68c94f8ced8e8f
Murilo <muriloomartins00@gmail.com>
2026-05-26 15:06:03 -0300

    visualsss

--- src/pages/enter/page.tsx
+++ src/pages/enter/page.tsx
@@ -35,8 +35,29 @@ export default function EnterPage() {
     return (
         <div className='flex w-full h-screen justify-center items-center text-white'>
 
-            <div className='flex flex-col gap-4 w-full max-w-[500px] items-center justify-center'>
-                <h1 className='text-[100px] font-medium'>Color Battle</h1>
+            <div className='flex flex-col gap-4 w-full max-w-[700px] items-center justify-center select-none'>
+                <h1 className='text-[150px] font-medium z-20 text-center'
+                    style={{
+                        color: 'transparent',
+                        backgroundClip: 'text',
+                        WebkitBackgroundClip: 'text',
+                        backgroundImage: 'linear-gradient(to right, #ff6666, #ff66ff, #6666ff, #66ffff, #66ff66, #ffff66, #ff6666)',
+
+                        backgroundSize: '200% auto',
+
+                        animation: 'gradient-x 4s linear infinite'
+                    }}>Color Battle</h1>
+                <h1 className='text-[150px] font-medium z-10 mt-[-240px] blur-2xl  text-center opacity-75'
+                    style={{
+                        color: 'transparent',
+                        backgroundClip: 'text',
+                        WebkitBackgroundClip: 'text',
+                        backgroundImage: 'linear-gradient(to right, #ff0000, #ff00ff, #0000ff, #00ffff, #00ff00, #ffff00, #ff0000)',
+
+                        backgroundSize: '200% auto',
+
+                        animation: 'gradient-x 4s linear infinite'
+                    }}>Color Battle</h1>
                 <div className='flex flex-col gap-2 w-full'>
                     <p className='text-[30px]'>seu Nome</p>
                     <input onChange={(e) => { setName(e.target.value) }} type="text" name="name" className='bg-white/10 hover:bg-white/15 transition-all 
